9d5301cd827e4c7ad48045ea829d6d6ce7120eea
kato2901 <seiten.hkss321@gmail.com>
2020-05-14 10:46:46 +0900

    クラスの定義

---
+++ Static2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person.printData(Person.fullName("kasei", "zin"), 20, 1.7, 55.0);
+        Person.printData(Person.fullName("reonarudo", "d", "vinchi"), 30, 1.9, 70.0);
+    }
+}
