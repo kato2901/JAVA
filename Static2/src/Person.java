e73a507225f3a020e3f143a3a0c52be6e1f6af0f
kato2901 <seiten.hkss321@gmail.com>
2020-05-19 10:37:39 +0900

    自己紹介

--- Static2/src/Person.java
+++ Static2/src/Person.java
@@ -1,6 +1,9 @@
 class Person {
-    public static void printData(String name, int age, double height, double weight) {
-        System.out.println("私の名前は" + name + "です。");
+    // public static void printData(String name, int age, double height, double weight)
+    public static void printData(String firstName, String lastName, int age, double height, double weight) {
+
+        System.out.println("名前は" + fullName(firstName, lastName) + "です");
+        // System.out.println("私の名前は" + name + "です。");
         System.out.println("年齢は" + age + "歳です。");
         System.out.println("身長は" + height + "mです。");
         System.out.println("体重は" + weight + "kgです。");
@@ -9,7 +12,7 @@ class Person {
         // 四捨五入
         System.out.println("BMIは" + Math.round(bmi) + "です");
 
-        if(isHealtyh(bmi)) {
+        if(isHealthy(bmi)) {
             System.out.println("標準値です");
         } else {
             System.out.println("標準値の範囲外です");
@@ -17,19 +20,18 @@ class Person {
     }
 
     public static String fullName(String firstName, String lastName) {
-        return firstName + "" + lastName;
+        return firstName + " " + lastName;
     }
 
-    public static String fullName(String firstName, String middlName, String lastName) {
-        return firstName + "" + middlName + "" + lastName;
-    }
+    // public static String fullName(String firstName, String middlName, String lastName) {
+    //     return firstName + "" + middlName + " " + lastName;
+    // }
 
     public static double bmi(double height, double weight) {
         return weight / height / height;
     }
 
-    public static boolean isHealtyh(double bmi) {
+    public static boolean isHealthy(double bmi) {
         return bmi >= 18.5 && bmi < 25.0;
     }
-
 }
